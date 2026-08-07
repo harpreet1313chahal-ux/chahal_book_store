269212b427373f16a1864311d1da3b472f040968
harpreet1313chahal-ux <harpreet1313chahal@gmail.com>
2026-08-07 00:28:13 -0500

    Fix test fixtures and pass model tests

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 class OrderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "order must have a valid status" do
+    order = Order.new(
+      status: "Invalid",
+      total_price: 100
+    )
+
+    assert_not order.valid?
+  end
+
+  test "order total price cannot be negative" do
+    order = Order.new(
+      status: "New",
+      total_price: -10
+    )
+
+    assert_not order.valid?
+  end
 end
