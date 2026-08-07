e22c410cade2a4785d630ab03fa1434d6121dd75
harpreet1313chahal-ux <harpreet1313chahal@gmail.com>
2026-08-07 06:57:10 -0500

    Add order status management in admin dashboard

--- app/admin/orders.rb
+++ app/admin/orders.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Order do
+
   permit_params :user_id,
                 :order_date,
                 :total_price,
@@ -8,4 +9,30 @@ ActiveAdmin.register Order do
                 :pst_rate,
                 :hst_rate,
                 :tax_amount
+
+  index do
+    selectable_column
+    id_column
+    column :user
+    column :status
+    column :total_price
+    column :created_at
+    actions
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :user
+      f.input :status, as: :select, collection: Order::STATUSES
+      f.input :total_price
+      f.input :shipping_address
+      f.input :gst_rate
+      f.input :pst_rate
+      f.input :hst_rate
+      f.input :tax_amount
+    end
+
+    f.actions
+  end
+
 end
