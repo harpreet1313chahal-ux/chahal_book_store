95cd3884acdd4fa64c3001c64b5d1a5d6294c474
harpreet1313chahal-ux <harpreet1313chahal@gmail.com>
2026-08-07 07:02:28 -0500

    Add province tax rate management in ActiveAdmin

---
+++ app/admin/provinces.rb
@@ -0,0 +1,26 @@
+ActiveAdmin.register Province do
+
+  permit_params :name, :gst_rate, :pst_rate, :hst_rate
+
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :gst_rate
+    column :pst_rate
+    column :hst_rate
+    actions
+  end
+
+  form do |f|
+  f.inputs do
+    f.input :name
+    f.input :gst
+    f.input :pst
+    f.input :hst
+  end
+
+  f.actions
+end
+
+end
